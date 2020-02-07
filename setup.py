b93abf4fee638c08a2659a70a23bf4ddefd41223
jlopezper <jorge.loper@gmail.com>
2020-02-07 18:17:32 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Example of a real ML project based on clustering',
+    author='Jorge Lopez',
+    license='MIT',
+)
